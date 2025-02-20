3ed048776d10aa8f13f650cf3b4364e19f41d39c
Filipos <fpapagewrgiou@synergic.gr>
2025-02-20 09:10:11 +0200

    started layout svelte for settings admin

---
+++ src/routes/settings/+layout.svelte
@@ -0,0 +1,13 @@
+<!-- routes/settings/+layout.svelte -->
+<script lang="ts">
+    import type { Snippet } from 'svelte';
+    import type { LayoutData } from './$types';
+    import Navbar from '$lib/components/ui/Navbar.svelte';
+    
+    let { data, children }: { data: LayoutData, children: Snippet } = $props();
+</script>
+
+<Navbar />
+<div class="container mx-auto px-4 mt-36">
+    {@render children()}
+</div>
